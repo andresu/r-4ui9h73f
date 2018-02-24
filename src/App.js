a73f008a36d71ea138030b71aef0704383d7ddae
Andres Urrea <andres.urreao@gmail.com>
2018-02-24 17:42:09 -0500

    Repeater-functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+        text: ""
+    };
+    this.handleRepeater = this.handleRepeater.bind(this); 
+  }
+
+  handleRepeater(ev){
+    this.setState({
+        text:ev.target.value
+    });
+  }  
+    
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.handleRepeater} />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
